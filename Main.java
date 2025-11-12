12c5ee73804b15cc379aac695efa05de84a3dc67
Yerasik <yerassylt@connect.hku.hk>
2025-11-12 15:52:45 +0800

    practicing //the first mortage calculator project

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Date;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.awt.*;
+
 public class Main{
   public static void main(String[] args){
-    System.out.println("Hello World");
+    final int YEARS = 10;
+    final double INTEREST = 0.2;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("You Principal = ");
+    int principal = scanner.nextInt();
+    double mortgage = principal * Math.pow(100+INTEREST, YEARS);
+    System.out.println("Your mortgage payments is " + mortgage);
   }
 }
