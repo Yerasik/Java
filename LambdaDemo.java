e164f5a7371ffff41361d3b7c93bb32ca6e39bdb
Yerasik <yerassylt@connect.hku.hk>
2025-11-14 17:17:27 +0800

    learning interfaces

---
+++ LambdaDemo.java
@@ -0,0 +1,18 @@
+
+
+
+public class LambdaDemo{
+  public static void show(){
+    greet(new Printer(){
+      @Override
+      public void print(String message){
+        System.out.println(message);
+      }
+    });
+    
+  }
+  public static void greet( Printer printer){
+    printer.print("Hello world");
+
+  }
+}
--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ import java.awt.*;
 
 public class Main{
   public static void main(String[] args){
-    ExceptionsDemo.show();
+    LambdaDemo.show();
   }
 }
