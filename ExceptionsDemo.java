74cc64c4500312d4771e252c92a612d05877bca4
Yerasik <yerassylt@connect.hku.hk>
2025-11-14 17:01:18 +0800

    learning how to handle multiple exceptions and try/ catch/ finally blocks

--- ExceptionsDemo.java
+++ ExceptionsDemo.java
@@ -1,17 +1,33 @@
 import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 
 public class ExceptionsDemo{
   public static void show(){
+    FileReader reader = null;
     try {
-      var reader = new FileReader("file.txt");
-      System.out.println("File Open");
-    } catch(FileNotFoundException ex){
-      System.out.println(ex.getMessage());
+      reader = new FileReader("file.txt");
+      var value = reader.read();
+      reader.close();
+    } 
+    catch(IOException e){
+      System.out.println("Could not read data");
     }
-
-   
+    finally{
+      if(reader!=null){
+        try{
+          reader.close();
+        }
+        catch(IOException e){
+          System.out.println("Didn't close.");
+        }
+        
+      }
+      System.out.println("Closed");
+    }
   }
   public static void sayHello(String name){
     System.out.println(name.toUpperCase());
